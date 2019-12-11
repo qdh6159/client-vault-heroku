83063b231debddba572d89aeebc5e777de161017
Quincy Hicks <qdh6159@gmail.com>
2019-12-10 19:48:06 -0600

    add back-end address

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 require('./db/db');
 
 // Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(session({
   secret: 'keyboard cat',
