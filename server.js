287a656e553caadd2411ce91e809052e032f68a6
Quincy Hicks <qdh6159@gmail.com>
2019-12-10 22:02:23 -0600

    move allow cors

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // SET UP CORS AS MIDDLEWARE, SO any client can make a request to our server
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://client-vault-heroku.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // CORS allows requests to come in from React
 app.use(cors({
@@ -26,11 +31,7 @@ app.use(cors({
   origin: process.env.REACT_ADDRESS
 }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://client-vault-heroku.herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 // app.use((req, res, next)=>{
 //   console.log(req.session.userId)
 //   next()
