00bd2850bcaedb40d891e08811138b89b4def753
Quincy Hicks <qdh6159@gmail.com>
2019-12-10 21:58:46 -0600

    add allow origin

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.use(cors({
   credentials: true,
   origin: process.env.REACT_ADDRESS
 }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://client-vault-heroku.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use((req, res, next)=>{
 //   console.log(req.session.userId)
 //   next()
